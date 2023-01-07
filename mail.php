73e7a38431733e72db02ea606f88370f31b62242
davidkkam <122059883+davidkkam@users.noreply.github.com>
2023-01-07 12:52:44 -0500

    01.07.22

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+//get data from form
+$name = $_POST['name']; 
+$email = $_POST['email'];
+$message = $_POST['message']; 
+$to = "dka_m@yahoo.com"; 
+$subject = "Mail from travel business";
+$txt = "Name = ". $name . "\r\n Email = " . $email . "\r\n
+	Mesage =" . $message;
+$headers = "From: noreply @david_kam.com" . "r\n" . 
+"CC: dka_m@yahoo.com";
+if($email!=NULL){
+	mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");
+?>
