23bb85548854961b8edcb8b5b3033db441265459
davidkkam <122059883+davidkkam@users.noreply.github.com>
2023-01-07 12:58:55 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -7,7 +7,7 @@ $to = "dka_m@yahoo.com";
 $subject = "Mail from travel business";
 $txt = "Name = ". $name . "\r\n Email = " . $email . "\r\n
 	Mesage =" . $message;
-$headers = "From: noreply @david_kam.com" . "r\n" . 
+$headers = "From: noreply@david_kam.com" . "r\n" . 
 "CC: dka_m@yahoo.com";
 if($email!=NULL){
 	mail($to,$subject,$txt,$headers);
